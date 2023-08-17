a9d46eab0b0999f14e093014be579c0ac04d74bf
Dariga Narmanova <122713640+dariiik@users.noreply.github.com>
2023-08-17 19:39:14 +0900

    Update view.py

--- view.py
+++ view.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 import requests
 from django.urls import path
+from django.http import JsonResponse
 from . import views
+#requesting acces to our html file
 def my_view(request):
     return render(
         request, 
@@ -10,11 +12,12 @@ def my_view(request):
 urlpatterns = [
     path('weather/', views.weather_view, name='weather'),
 ]
+#implementing our api
 def weather_view(request): 
     city_value = request.GET.get('city', 'VARANASI')
-    api_key = "2f745fa85d563da5adb87b6cd4b81caf"
+    api_key= "2f745fa85d563da5adb87b6cd4b81caf"
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city_value}&appid={api_key}&units=metric"
-
+#server-side api
     try: 
         response = requests.get(url)
         data = response.json()
@@ -25,4 +28,4 @@ def weather_view(request):
         context = {
             'error': 'City not found',
         }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html', context)    
